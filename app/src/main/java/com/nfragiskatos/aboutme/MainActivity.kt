bf1f13a69fcc0a4bc2b2029618c7cb9826cdb01c
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2019-09-22 16:13:29 -0700

    Hooked up done button to an onClickListener
    - assigns the value in the edit text to the display text field
    - hide the done button
    - hide edit text
    - make the display text field visible
    - hide input keyboard

--- app/src/main/java/com/nfragiskatos/aboutme/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/aboutme/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.nfragiskatos.aboutme
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +14,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener(this::addNickname)
+    }
+
+    private fun addNickname(btn: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        btn.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(btn.windowToken, 0)
     }
 }
