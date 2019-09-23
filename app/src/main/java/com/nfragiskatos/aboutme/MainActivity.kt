1b24dd001dd9db39adc40d9ab89faf60f2aa19ec
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2019-09-23 16:41:04 -0700

    Introduced data binding to the app, and started using the binding object instead of findViewById

--- app/src/main/java/com/nfragiskatos/aboutme/MainActivity.kt
+++ app/src/main/java/com/nfragiskatos/aboutme/MainActivity.kt
@@ -8,24 +8,30 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.nfragiskatos.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aaron Rodgers")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener(this::addNickname)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener(this::addNickname)
     }
 
     private fun addNickname(btn: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        btn.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
         // Hide keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
---
+++ app/src/main/java/com/nfragiskatos/aboutme/MyName.kt
@@ -0,0 +1,8 @@
+package com.nfragiskatos.aboutme
+
+import android.provider.ContactsContract
+
+/**
+ * Created by Nicholas Fragiskatos on 9/22/2019.
+ */
+data class MyName(var name: String = "", var nickname: String = "")
